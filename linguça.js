c505118ed0bae246152e0fed907627658385960b
uener <uperesmarcon@gmail.com>
2024-10-08 15:30:31 -0300

    uener

---
+++ linguça.js
@@ -0,0 +1,10 @@
+class Linguca {
+    constructor(tipo, peso, tamanho, dataFabricacao, dono, nivelQualidade) {
+        this.tipo = tipo
+        this.peso = peso
+        this.tamanho = tamanho
+        this.dataFabricacao = dataFabricacao
+        this.dono = dono
+        this.nivelQualidade = nivelQualidade
+    }
+}
